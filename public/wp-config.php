edc1b82a1fae1815b9a9b5a3e8bfa7072cae2d5c
Raúl Uranga <ruranga@gmail.com>
2018-06-07 14:30:19 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -100,7 +100,7 @@ define('WP_DEBUG', false);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/cms');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
